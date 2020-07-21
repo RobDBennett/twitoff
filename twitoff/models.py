5668fbbce79b8eb54a7be699d36b7035b15a5acc
RobDBennett <robbennett.work@gmail.com>
2020-07-21 11:05:11 -0700

    Added connection to basilica and twitter API, and enviornment keys.

--- twitoff/models.py
+++ twitoff/models.py
@@ -2,19 +2,35 @@
 from flask_sqlalchemy import SQLAlchemy
 
 TWITTER_USERS = ['calebhicks', 'elonmusk', 'rrherr', 'SteveMartinToGo',
-                'alyankovic', 'nasa', 'sadserver', 'jkhowland', 'austen',
-                'common_squirrel', 'KenJennings', 'conanobrien', 'big_ben_clock',
-                'IAM_Shakespeare']
+                 'alyankovic', 'nasa', 'sadserver', 'jkhowland', 'austen',
+                 'common_squirrel', 'KenJennings', 'conanobrien', 'big_ben_clock',
+                 'IAM_Shakespeare']
 
 DB = SQLAlchemy()
+
 
 class User(DB.Model):
     """Twitter users."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
-
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    
     def __repr__(self):
         return 'User {}'.format(self.name)
+
+
+class Tweet(DB.Model):
+    """Tweets and their embeddings from Basilica."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # 300 allows it to have links.
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
+        'user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '[Tweet {}]'.format(self.text)
+
 
 def add_test_users():
     for i, name in enumerate(TWITTER_USERS):
